877346f2726048ecd68fc15da0cfcacbe721f5e6
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-05-17 01:51:06 +0300

    add tasks

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,5 @@
-export const PATH_DB =
+import path from 'node:path';
+// import fs from 'fs/promises';
+export const PATH_DB = path.join('src', 'db', 'db.json');
+// export const prevContacts = await fs.readFile(PATH_DB);
+// export const contacts = JSON.parse(prevContacts);
